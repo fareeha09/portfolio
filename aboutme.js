cedc66e101384c4e8982ac22dfd783be23a042f4
feraboo <ftabassum@stuy.edu>
2017-08-01 10:07:05 -0400

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,1 @@
-<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
-
-<img id="myImage" src="pic_bulboff.gif" style="width:100px">
-
-<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
+windows.alert("You are about to enter a world of chaos");
