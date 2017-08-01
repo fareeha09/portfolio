f09182f8493f93e51bca2da01009c3a0072e0410
feraboo <ftabassum@stuy.edu>
2017-08-01 10:26:13 -0400

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -1,1 +1,5 @@
 window.alert("You are about to enter a world of chaos");
+
+function changeBack() {
+document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/commons/d/d5/Pic_de_neige_cordier_Face_E.jpg";
+}
