1e2b7e8700a016394afa090a87b3b4dd999f1490
feraboo <ftabassum@stuy.edu>
2017-08-01 11:02:38 -0400

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,10 @@
 window.alert("You are about to enter a world of chaos");
 
 function changePic() {
-document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/commons/d/d5/Pic_de_neige_cordier_Face_E.jpg";
+var counter=0;
+if (counter%2=0){
+  document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/commons/d/d5/Pic_de_neige_cordier_Face_E.jpg";
+} else {
+  document.getElementById("image").src = "http://www.nicoleisthenewblack.com/wp-content/uploads/2011/09/vintage-hand-about-me.png";
 }
+counter+=1
